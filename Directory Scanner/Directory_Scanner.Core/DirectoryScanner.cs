b3db4078f7fcf302f9cdcecc96b570d603c5accc
NotDmitry <91372817+NotDmitry@users.noreply.github.com>
2022-12-18 18:59:10 +0300

    New feature
    Added basic implementation of directory scanner with constructor and method for obtaining root node of file tree

---
+++ Directory Scanner/Directory_Scanner.Core/DirectoryScanner.cs
@@ -0,0 +1,47 @@
+﻿using Directory_Scanner.Model.Threads;
+using Directory_Scanner.Model.Tree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Directory_Scanner.Model;
+
+public class DirectoryScanner
+{
+	private int maxThreads;
+	private readonly int _defaultThreads = 6;
+    public int MaxThreads 
+	{
+		get => maxThreads;
+		private set
+		{
+			maxThreads = value > 0 ? value : _defaultThreads; 
+		}
+	}
+
+	private TaskQueue? _taskQueue;
+	private TreeNode? _root;
+
+	public DirectoryScanner(int maxThreads)
+	{
+		_root = null;
+		_taskQueue = null;
+		MaxThreads = maxThreads;
+	}
+
+	public FileSystemTree StartScanning(string rootPath)
+	{
+		if (Directory.Exists(rootPath))
+		{
+			var file = new FileInfo(rootPath);
+		}
+		else
+			throw new Exception("This directory does not exist");
+
+		return new FileSystemTree(_root);
+	}
+
+
+}
